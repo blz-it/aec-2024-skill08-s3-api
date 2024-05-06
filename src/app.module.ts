ccecef25ff3c3c942702cdd6914ddcbc4867eb09
Benjamin Frost <bfrost.schule@gmail.com>
2024-05-06 17:27:55 +0200

    feat: serve upload content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), UsersModule, AuthModule],
+  imports: [
+    AuthModule,
+    MikroOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
