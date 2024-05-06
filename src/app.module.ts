50c92140c064c900414adf1eca8a8a868a461d5e
Benjamin Frost <bfrost.schule@gmail.com>
2024-05-06 13:32:47 +0200

    feat: add mikro orm and db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'postgres',
+      user: 'postgres',
+      password: 'postgres',
+      driver: PostgreSqlDriver,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
