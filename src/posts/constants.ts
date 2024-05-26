fc67facdcc5254fb8333cf01a8623b9754ba4848
Benjamin Frost <bfrost.schule@gmail.com>
2024-05-26 22:54:12 +0200

    feat: add new stickers

--- src/posts/constants.ts
+++ src/posts/constants.ts
@@ -1,6 +1,30 @@
 export const stickers = [
   {
-    name: 'jonas',
-    imageUrl: '/static/stickers/jonas.png',
+    name: 'android',
+    imageUrl: '/static/stickers/android.png',
+  },
+  {
+    name: 'apple',
+    imageUrl: '/static/stickers/apple.png',
+  },
+  {
+    name: 'dart',
+    imageUrl: '/static/stickers/dart.png',
+  },
+  {
+    name: 'flutter',
+    imageUrl: '/static/stickers/flutter.png',
+  },
+  {
+    name: 'java',
+    imageUrl: '/static/stickers/java.png',
+  },
+  {
+    name: 'kotlin',
+    imageUrl: '/static/stickers/kotlin.png',
+  },
+  {
+    name: 'swift',
+    imageUrl: '/static/stickers/swift.png',
   },
 ];
